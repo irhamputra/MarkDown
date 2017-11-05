09f29b9951e90557c6816aa0c21ae5cf51bb3213
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-05 01:15:04 +0100

    Navigation link tag

--- client/components/header.js
+++ client/components/header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Accounts from './accounts';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -13,7 +14,7 @@ class Header extends Component {
         return(
             <nav className="nav navbar-inverse">
                 <div className="navbar-header">
-                    <a className="navbar-brand">MarkBin</a>
+                    <Link to="/" className="navbar-brand">MarkBin</Link>
                 </div>
                 <ul className="nav navbar-nav">
                     <li>

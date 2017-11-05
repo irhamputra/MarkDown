ddb5ac6544145b2464adb2ca7a5b226f36a375e2
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-05 02:07:31 +0100

    install codemirror and implementing markdown editor

--- client/components/bins/bins_editor.js
+++ client/components/bins/bins_editor.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';
 
 class BinEditor extends Component {
     render(){
         return(
-            <div>Bins Editor</div>
+            <div className="col-xs-8">
+                <h4>Input Code</h4>
+                <CodeMirror options={{ mode: 'markdown', lineNumbers: true }} />
+            </div>
         )
     }
 }

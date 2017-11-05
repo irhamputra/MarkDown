4d0e111b9514fce1d951f18e5797daec15ea4b3a
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-05 03:10:53 +0100

    implementing sharedWith object containing email

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -3,13 +3,11 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
 import BinsViewer from './bins_viewer';
-
+import BinsShare from './bins_share';
 
 class BinsMain extends Component{
     render() {
-
         if(!this.props.bin) { return <div className="text-hide">Loading...</div>; }
-
         return(
             <div className="container-fluid">
                 <br/>
@@ -18,6 +16,7 @@ class BinsMain extends Component{
                 </div>
                 <BinsEditor bin={this.props.bin}/>
                 <BinsViewer bin={this.props.bin}/>
+                <BinsShare bin={this.props.bin}/>
             </div>
         )
     }

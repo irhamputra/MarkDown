b84e3252fce00ad5ffe7b3dc0107f2e411958795
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-05 02:01:26 +0100

    creating markdown editor

---
+++ client/components/bins/bins_editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BinEditor extends Component {
+    render(){
+        return(
+            <div>Bins Editor</div>
+        )
+    }
+}
+
+export default BinEditor;
--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
+import BinsEditor from './bins_editor';
 
 class BinsMain extends Component{
     render() {
-        console.log(this.props.bin);
         return(
-            <div className="container">BinsMain</div>
+            <div className="container">
+                <BinsEditor bin={this.props.bin}/>
+            </div>
         )
     }
 }
